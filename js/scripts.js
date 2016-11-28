64217340c9d473699ed9959d5d4e65d83356f42a
Yous15 <a16josortmar@iam.cat>
2016-11-28 23:30:24 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+function w3_open() {
+  document.getElementById("divmain").style.marginLeft = "25%";
+  document.getElementById("mySidenav").style.width = "25%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function w3_close() {
+  document.getElementById("divmain").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+  document.body.style.backgroundColor = "white";
+}
+
+function w3_openF() {
+  document.getElementById("divmain").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("formulario").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function w3_closeF() {
+  document.getElementById("divmain").style.marginLeft = "0%";
+  document.getElementById("formulario").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+  document.body.style.backgroundColor = "white";
+}
+
+function w3_open() {
+  document.getElementById("divmain").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function w3_close() {
+  document.getElementById("divmain").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+  document.body.style.backgroundColor = "white";
+}
+
+function w3_openO() {
+  document.getElementById("divmain").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("paisajes").style.marginLeft = "15%";
+  document.getElementById("paisajes2").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function w3_closeO() {
+  document.getElementById("divmain").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("paisajes").style.marginLeft = "0%";
+  document.getElementById("paisajes2").style.marginLeft = "0%";
+  document.getElementById("openNav").style.display = "inline-block";
+  document.body.style.backgroundColor = "white";
+}
+
+function w3_openT() {
+  document.getElementById("divmain").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("tradicion").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function w3_closeT() {
+  document.getElementById("divmain").style.marginLeft = "0%";
+  document.getElementById("tradicion").style.marginLeft = "0%"
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+  document.body.style.backgroundColor = "white";
+}
